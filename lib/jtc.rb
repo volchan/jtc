22a389f5cb2d5edd9ecc8e0d5704a1951a490ea2
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-12-26 15:58:56 +0100

    Convert JTC class to module

--- lib/jtc.rb
+++ lib/jtc.rb
@@ -3,52 +3,12 @@
 require 'csv'
 require 'json'
 
-class JTC
-  attr_reader :json
+require_relative 'jtc/convertor'
 
-  private
-
-  attr_accessor :headers
-
-  public
-
+module JTC
   class <<self
     def convert(json)
-      new(json).call
-    end
-  end
-
-  def initialize(json)
-    @json = json
-    @headers = nil
-  end
-
-  def call
-    parsed_json = JSON.parse(json)
-    self.headers = generate_csv_header(parsed_json.first)
-
-    CSV.generate do |csv|
-      csv << headers
-      parsed_json.each do |object|
-        csv << generate_csv_line(object)
-      end
-    end
-  end
-
-  private
-
-  def generate_csv_header(json)
-    json.flat_map do |key, value|
-      next key unless value.is_a?(Hash)
-
-      generate_csv_header(value).map { |head| [key, head].join('.') }
-    end
-  end
-
-  def generate_csv_line(object)
-    headers.map do |header|
-      value = header.split('.').reduce(object) { |obj, key| obj[key] }
-      value.is_a?(Array) ? value.join(',') : value
+      Convertor.new(json).call
     end
   end
 end
---
+++ lib/jtc/convertor.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module JTC
+  class Convertor
+    attr_reader :json
+
+    private
+
+    attr_accessor :headers
+
+    public
+
+    def initialize(json)
+      @json = json
+      @headers = nil
+    end
+
+    def call
+      parsed_json = JSON.parse(json)
+      self.headers = generate_csv_header(parsed_json.first)
+
+      CSV.generate do |csv|
+        csv << headers
+        parsed_json.each do |object|
+          csv << generate_csv_line(object)
+        end
+      end
+    end
+
+    private
+
+    def generate_csv_header(json)
+      json.flat_map do |key, value|
+        next key unless value.is_a?(Hash)
+
+        generate_csv_header(value).map { |head| [key, head].join('.') }
+      end
+    end
+
+    def generate_csv_line(object)
+      headers.map do |header|
+        value = header.split('.').reduce(object) { |obj, key| obj[key] }
+        value.is_a?(Array) ? value.join(',') : value
+      end
+    end
+  end
+end
